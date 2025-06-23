5ace8a407391918d5814408294d8cbddbb7b27b3
RASHMI RANJAN DALAI <ranjanrashmi611@gmail.com>
2025-06-23 08:05:41 +0530

    Callbacks and Higher Order Functions

--- ep05/index.js
+++ ep05/index.js
@@ -30,4 +30,70 @@ sumOfTwoNumbers(5,7); //12
 
 //callback function
 //it depends on some other function
+//when we pass a function as parameters in some other function and receive as an argument 
+//js is single thread means it executes the code line by line
 
+// const doSomething = function(callback) {
+//   console.log("samose ban rahe hai..");
+//   setTimeout(() => {
+//     callback();
+//   }, 3000);
+//   console.log("almost ready hai")
+// }
+
+// const ready = function(){
+//   console.log("samose ready hai");
+// }
+
+// doSomething(ready);
+
+
+
+//Higher order function
+const radius = [1,2,3,4];
+
+const areas = function(radiusArray){
+  const result = [];
+  for(let i = 0; i<radiusArray.length; i++){
+    const calculate = Math.PI * radiusArray[i] * radiusArray[i];
+    result.push(calculate);
+  }
+  return result;
+}
+
+const ansArea = areas(radius);
+console.log(ansArea);
+
+
+const circumferences = function(radiusArray){
+  const result = [];
+  for(let i = 0; i<radiusArray.length; i++){
+    const calculate = 2 * Math.PI * radiusArray[i];
+    result.push(calculate);
+  }
+  return result;
+}
+
+const ansCircumference = circumferences(radius);
+console.log(ansCircumference);
+
+//to avoid the above repeating things, because here the only differnce is in formula -> we use higheer order function
+
+const circumFormula = function(radii){
+  return 2 * Math.PI * radii;
+}
+
+const areaFormula = function(radii){
+  return Math.PI * radii * radii;
+}
+
+const calculate = function(radius, Formula) {
+  let a = [];
+  for(let i = 0; i<radius.length; i++){
+    a.push(Formula(radius[i]));
+  }
+  return a;
+}
+
+console.log(calculate(radius, areaFormula));
+console.log(calculate(radius, circumFormula));
